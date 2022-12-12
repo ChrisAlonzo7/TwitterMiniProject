75d106589bb4201c7b2377aa8f9822c344e92a93
ChrisAlonzo7 <73143256+ChrisAlonzo7@users.noreply.github.com>
2022-12-12 10:58:01 -0500

    Update Botometer.py
    removing keys

--- Botometer.py
+++ Botometer.py
@@ -1,11 +1,11 @@
 import botometer
 
-rapidapi_key = "edbd4ef82cmsh451f77e71d3ce33p16e014jsn78dd"
+rapidapi_key = ""
 twitter_app_auth = {
-    'consumer_key': '7JR2f1PlJoXlAdQTrunkfXs5J',
-                    'consumer_secret': '5vhD76MR6JPa2DUU5r4DKePoJVN4LhnLGc5BMA9ojiVXWGOoED',
-                    'access_token': '1569770373687967744-wyHHAosi5GbOJMk3ry5JMHZh6hYQJe',
-                    'access_token_secret': 'LtbE9OKi7oNgcBHfeBQRUJWy6djj7kndaCq8LSc0uU8IR',
+    'consumer_key': '',
+                    'consumer_secret': '',
+                    'access_token': '',
+                    'access_token_secret': '',
   }
 bom = botometer.Botometer(wait_on_ratelimit=True,
                           rapidapi_key=rapidapi_key,
